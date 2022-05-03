54b74a10c05bd3aca7fa1a2bba114856e104ba80
Woshikuzzaman Anik <woshikuzzaman@gmail.com>
2022-05-04 00:20:03 +0600

    # Solve - element duplicate

---
+++ 217-contains-duplicate.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsDuplicate(self, nums):
+        hashset = set()
+
+        for num in nums:
+          if num in hashset:
+            return True
+          
+          hashset.add(num)
+        
+        return False
