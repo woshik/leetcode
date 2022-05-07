3356b62733d3ef93aa14ba246f575be0036853fa
Woshikuzzaman Anik <woshikuzzaman@gmail.com>
2022-05-07 23:58:27 +0600

    # Solve - palindrome partitioning

---
+++ 131-palindrome-partitioning.py
@@ -0,0 +1,21 @@
+class Solution:
+    def partition(self, s):
+      result = []
+      self.calculatePartition(s, [], result)
+      return result
+
+    def calculatePartition(self, sequence, container, result):
+      if len(sequence) == 0:
+        result.append(container[:])
+
+      
+      for index in range(1, len(sequence)+1):
+        tempInput = sequence[:index]
+        if tempInput == tempInput[::-1]:
+          container.append(tempInput)
+          self.calculatePartition(sequence[index:], container, result)
+          container.remove(tempInput)
+        
+s = Solution()
+
+print(s.partition("aab"))
